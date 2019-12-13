2e8c7613effe26105a138124bd2e4f73d8dccef4
Alejandro AR <alex-ar@outlook.com>
2019-12-13 16:43:30 -0600

    Updates

---
+++ precache-manifest.2109e06137f0ee4d713e93bbb429a110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc7a7a948170280d96e003a5aa4afdc",
+    "url": "/Alexsaurio/HotBoxTimer/index.html"
+  },
+  {
+    "revision": "581e39f69977989f698d",
+    "url": "/Alexsaurio/HotBoxTimer/static/css/main.e2f6d472.chunk.css"
+  },
+  {
+    "revision": "79c93ffd8ff6568e3eca",
+    "url": "/Alexsaurio/HotBoxTimer/static/js/2.d5791620.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/Alexsaurio/HotBoxTimer/static/js/2.d5791620.chunk.js.LICENSE"
+  },
+  {
+    "revision": "581e39f69977989f698d",
+    "url": "/Alexsaurio/HotBoxTimer/static/js/main.f9a34b51.chunk.js"
+  },
+  {
+    "revision": "cd70023aed320b9c939b",
+    "url": "/Alexsaurio/HotBoxTimer/static/js/runtime-main.82f868fd.js"
+  },
+  {
+    "revision": "72d20c1915dee2a30c56b31d4449bf5b",
+    "url": "/Alexsaurio/HotBoxTimer/static/media/imagen.72d20c19.png"
+  }
+]);
