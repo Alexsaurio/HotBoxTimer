ab8ba0bbf03c2f0fd197bf3f38455446b1f8bf95
Alejandro AR <alex-ar@outlook.com>
2019-12-13 16:39:40 -0600

    reloj v1

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from '../shared/logo.svg';
+import logo from '../shared/imagen.png';
 import './App.css';
+
+import Reloj from './reloj/reloj'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Reloj time={50}/>
       </header>
     </div>
   );
---
+++ src/components/reloj/reloj.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+
+import { Grid, ButtonGroup, Button } from '@material-ui/core';
+
+
+class Reloj extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            M: this.props.time,
+            S: 0,
+            Style: 'Clock',
+            pause: false
+        };
+
+        this.handleClickIniciar = this.handleClickIniciar.bind(this);
+        this.handleClickReiniciar = this.handleClickReiniciar.bind(this);
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        if (this.state.pause) {
+            const { M, S } = this.state;
+            if (M === this.props.time / 2)
+                this.setState({ Style: 'Clock Hurry' });
+            else if (M === this.props.time / 5)
+                this.setState({ Style: 'Clock Hurryx2' });
+
+            if (S === 0 && M !== 0) {
+                this.setState({ S: 60 });
+                this.setState({ M: (this.state.M - 1) });
+            }
+            if (this.state.M === 0 && this.state.S === 0) {
+                this.setState({ S: 0 });
+                this.setState({ M: 0 });
+                this.setState({ active: false });
+            } else
+                this.setState({ S: (this.state.S - 1) });
+        }
+    }
+
+    handleClickIniciar(){
+       this.setState({ pause: !this.state.pause });
+    }
+
+    handleClickReiniciar(){
+       this.setState({ M: this.props.time});
+       this.setState({ S: 0});
+       this.setState({ pause: false });
+    }
+
+
+
+    render() {
+        const { M, S, Style } = this.state;
+        return (
+            
+            <Grid>
+                <div className={Style}> {M}:{S}</div>
+                <ButtonGroup color="secondary" aria-label="outlined primary button group">
+                    <Button onClick={this.handleClickIniciar}>Iniciar</Button>
+                    <Button onClick={this.handleClickReiniciar}>Reinciar</Button>
+                </ButtonGroup>
+            </Grid>
+        );
+    }
+}
+
+export default Reloj;
